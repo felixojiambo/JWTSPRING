cc1fd3eb3c2b8ec9d11fad4a730985117bb8ccd4
Felix Ojiambo <felixojiamboe@gmail.com>
2024-03-11 07:26:34 -0700

    jwt service

---
+++ src/main/java/com/zep/jwt/config/JWTService.java
@@ -0,0 +1,12 @@
+package com.zep.jwt.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+
+    public String extractUsername(String jwt) {
+        return  null;
+    }
+}
