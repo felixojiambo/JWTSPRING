4b7e1c34b7596b052c5bf3ed859f71f25dc1d1b7
Felix Ojiambo <felixojiamboe@gmail.com>
2024-03-11 08:59:04 -0700

    jwt services

--- src/main/java/com/zep/jwt/config/JWTService.java
+++ src/main/java/com/zep/jwt/config/JWTService.java
@@ -1,12 +1,58 @@
 package com.zep.jwt.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
 
+    @Value("${encryption.secretKey}")
+    private String secretKey;
+    public String extractUsername(String token) {
+        return  extractClaim(token, Claims::getSubject);
+    }
 
-    public String extractUsername(String jwt) {
-        return  null;
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver)
+    {
+        final  Claims claims=extractAllClaims(token);
+        return  claimsResolver.apply(claims);
+
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public  String generateToken(
+            Map<String, Object> extractClaims,
+            UserDetails userDetails
+    ){
+        return  Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .subject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() +1000*60*24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+    private Key getSignInKey(){
+        byte[] keyBytes= Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
